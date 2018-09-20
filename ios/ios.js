907e913760fd5b91e8fe5d2a41a4f903ba588726
Bobby Dilley <b-dilley@criticalsoftware.com>
2018-09-20 10:08:22 +0100

    Prevent a touch start

--- ios/ios.js
+++ ios/ios.js
@@ -1,3 +1,17 @@
 window.onload = function() {
   window.scrollBy(0, 30);
 }
+
+var firstMove;
+
+window.addEventListener('touchstart', function (e) {
+    firstMove = true;
+});
+
+window.addEventListener('touchmove', function (e) {
+    if (firstMove) {
+        e.preventDefault();
+
+        firstMove = false;
+    }
+});
