edab2d35bed9738789563980c5b23d3fd70b29f5
Bobby Dilley <b-dilley@criticalsoftware.com>
2018-09-20 10:16:58 +0100

    Issues with author

--- ios/ios.js
+++ ios/ios.js
@@ -2,17 +2,17 @@ var firstMove;
 window.onload = function() {
   // Scroll the window
   window.scrollBy(0, 30);
-
-  // Stop anyone now scrolling the window
-  window.addEventListener('touchstart', function (e) {
-      firstMove = true;
-  });
-
-  window.addEventListener('touchmove', function (e) {
-      if (firstMove) {
-          e.preventDefault();
-
-          firstMove = false;
-      }
-  });
 }
+
+// Stop anyone now scrolling the window
+window.addEventListener('touchstart', function (e) {
+    firstMove = true;
+});
+
+window.addEventListener('touchmove', function (e) {
+    if (firstMove) {
+        e.preventDefault();
+
+        firstMove = false;
+    }
+});
