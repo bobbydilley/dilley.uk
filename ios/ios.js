64b8d6700d7aad99271064c06b2173679909c210
Bobby Dilley <bobbydilley@live.com>
2018-09-20 10:35:05 +0100

    See if moving with js helps

--- ios/ios.js
+++ ios/ios.js
@@ -2,6 +2,6 @@ var firstMove;
 window.onload = function() {
   // Scroll the window
   window.scrollTo(0, 30);
-
+  document.getElementById('meme').style.top = document.getElementById('meme').style.top - 40;
 
 }
