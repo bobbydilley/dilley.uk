ec1ee166d1c9c7b55ab9c9dd15f3ebbf7769d849
Bobby Dilley <bobbydilley@live.com>
2018-09-20 10:29:52 +0100

    Trying fixeds

--- ios/ios.js
+++ ios/ios.js
@@ -2,36 +2,6 @@ var firstMove;
 window.onload = function() {
   // Scroll the window
   window.scrollTo(0, 30);
-  // Stop anyone now scrolling the window
-  window.addEventListener('touchstart', function (e) {
-    e.preventDefault();
-      firstMove = true;
-      window.scrollTo(0, 30);
-  });
 
-  window.addEventListener('touchmove', function (e) {
-    e.preventDefault();
-      if (firstMove) {
-          e.preventDefault();
-          firstMove = false;
-      }
-      window.scrollTo(0, 30);
-  });
 
 }
-
-// Stop anyone now scrolling the window
-window.addEventListener('touchstart', function (e) {
-  e.preventDefault();
-    firstMove = true;
-    window.scrollTo(0, 30);
-});
-
-window.addEventListener('touchmove', function (e) {
-  e.preventDefault();
-    if (firstMove) {
-        e.preventDefault();
-        firstMove = false;
-    }
-    window.scrollTo(0, 30);
-});
