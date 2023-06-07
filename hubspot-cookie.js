ef03f4a23f49bae507e087b601b3e1369372a663
Bobby Dilley <bobbydilley@users.noreply.github.com>
2023-06-07 16:04:25 +0100

    Update hubspot-cookie.js

--- hubspot-cookie.js
+++ hubspot-cookie.js
@@ -1,1 +1,16 @@
-console.log('Bobby Dilley 2023');
+// Define the hubspot variable to push to
+var _hsp = window._hsp = window._hsp || [];
+
+// Define the gtag function
+window.dataLayer = window.dataLayer || [];
+function gtag() { window.dataLayer.push(arguments); }
+
+// Grant Google Tag Manager consent when HubSpot consent hook is fired
+_hsp.push(['addPrivacyConsentListener', function (consent) {
+    if (consent.allowed) {
+        gtag('consent', 'update', {
+            'analytics_storage': 'granted',
+            'ad_storage': 'granted',
+        });
+    }
+}]);
